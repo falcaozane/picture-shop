41f253a11474cacdb2ac250b160e6b27881aee8c
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-05-08 19:34:03 +0530

    navbar update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
+import { FaHome, FaShoppingCart } from 'react-icons/fa'; // Import FontAwesome icons
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white">
-      <ul className="flex justify-between">
-        <li>
-          <Link href="/">Home</Link>
+      <ul className="flex justify-between items-center"> {/* Ensure items are centered */}
+        <li className="flex items-center">
+          <FaHome className="mr-2" /> {/* Add icon with spacing */}
+          <Link href="/">Home</Link> {/* Maintain text link */}
         </li>
-        <li>
-          <Link href="/cart">Cart</Link>
+        <li className="flex items-center">
+          <FaShoppingCart className="mr-2" /> {/* Add cart icon with spacing */}
+          <Link href="/cart">Cart</Link> {/* Maintain text link */}
         </li>
       </ul>
     </nav>
