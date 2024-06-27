889474480281666e3569e81684abf82020634214
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2024-06-27 19:58:09 +0530

    infinite scroll

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { useCart } from '@/app/context/cartContext';
